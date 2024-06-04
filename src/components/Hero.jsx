f299d1792929567961602344bd99601853e3f516
Shiv-468 <shivmuwal.147@gmail.com>
2024-06-04 09:17:06 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+  <section className=' min-vh-100'>
+    <div className=" container">
+      
+    </div>
+  </section>
+  )
+}
+
+export default Hero
